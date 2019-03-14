013651af33c52dd5ac0739379d8ec5deebc95c4d
yinbodotcc <yinbodotcc@aliyun.com>
2019-03-14 22:25:15 +0800

    the initial edition

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		int a = 1;
+		System.out.println("hello,yay");
+	}
+	
+	public static int add(int a, int b)
+	{
+		return a + b;
+	}
+	
+	public static int sub(int a, int b)
+	{
+		return a - b;
+	}
+
+}
